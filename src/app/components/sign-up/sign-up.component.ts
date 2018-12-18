4955ebc6216633ae67f87dc270da8342747dba12
Vince <vincent_nronald@yahoo.com>
2018-12-18 19:58:52 +0900

    premiere ebauche de la page d'enregistrement

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Freelance} from '../../interfaces/freelance'
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
+  step:number=1;
+  freelance:Freelance;
   constructor() { }
 
   ngOnInit() {
+  }
+  next(){
+    this.step+=1;
   }
 
 }
